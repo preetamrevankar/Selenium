140df9b2bcc5f50568485d8c1afdcbbbb8c9b342
preetamrevankar <preetam.revankar@juspay.in>
2025-08-24 19:09:38 +0530

    selenium project

---
+++ src/test/java/com/Preetam/basictestcase.java
@@ -0,0 +1,15 @@
+package com.Preetam;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class basictestcase {
+    @Test
+    public static void testcase () {
+        Assert.assertEquals("ptrram","ptrram");
+
+
+    }
+
+}
