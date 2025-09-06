a30ca14d55b6247e3f3bd78bfea3ba19debdbe7d
preetamrevankar <preetam.revankar@juspay.in>
2025-09-07 03:19:37 +0530

    selenium project1

--- src/test/java/com/Preetam/sampletestcase.java
+++ src/test/java/com/Preetam/sampletestcase.java
@@ -4,11 +4,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+@Test
 public class sampletestcase {
     public static void main(String[] args) {
-        WebDriver pree = new FirefoxDriver();
+        WebDriver pree = new ChromeDriver();
+        pree.manage().window().maximize();
         pree.get("https://www.selenium.dev/documentation/webdriver/browsersk");
+//        Assert.assertEquals(driver.);
+      pree.navigate().forward();
+      pree.navigate().refresh();
+      pree.navigate().back();
+
 
     }
 }
